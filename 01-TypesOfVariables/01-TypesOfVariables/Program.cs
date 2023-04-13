39310d193587ea6ce4020e07eec30fcc043fd113
Johan Fonseca <jfonseca@grupocondal.cr>
2023-04-12 18:39:26 -0600

    primera proyecto de la practica del curso, hecho con c# console

---
+++ 01-TypesOfVariables/01-TypesOfVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information   
+using System.Numerics;
+
+//Console.WriteLine("Hello, World!");
+
+//programa de pruebam ejercicio de variables
+//Declaración y tipo de variables
+
+string name = "Miguel";
+int    age = 20;
+Double salary = 200.50;
+//El tipo decimal tiene un intervalo más pequeño, pero mayor precision que un double
+decimal disccount = 100.50m;
+bool isEmployee = true;
+
+
+Console.WriteLine($" Nombre: {name}\n Edad:{age}\n Empleado: {isEmployee}\n Salario: {salary}\n Descuento {disccount}");
