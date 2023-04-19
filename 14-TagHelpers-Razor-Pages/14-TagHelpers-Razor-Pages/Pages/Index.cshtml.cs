4d7a4e64202f3346ac499d3ea08e4330e9417f79
Johan Fonseca <jfonseca@grupocondal.cr>
2023-04-19 17:20:44 -0600

    ejercicio 14

---
+++ 14-TagHelpers-Razor-Pages/14-TagHelpers-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _14_TagHelpers_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Movie Movie { get; set; }    
+        public void OnGet()
+        {
+            Movie = new Movie 
+            {
+                Id = 1,
+                Title = "Godzilla X Kong",
+                Genre = "Accion"
+            };
+        }
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+    }
