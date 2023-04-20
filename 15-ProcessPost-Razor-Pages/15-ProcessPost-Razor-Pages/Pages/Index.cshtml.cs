cdedf45ac3bd4eee4ad4bbdc50c1cca05b10b2f5
Johan Fonseca <jfonseca@grupocondal.cr>
2023-04-19 18:00:59 -0600

    ejercicio 15 post

---
+++ 15-ProcessPost-Razor-Pages/15-ProcessPost-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _15_ProcessPost_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]// este atributo se utiliza para simplificar el proceso de vinculacion de datos entre el PageModel y un formulario
+    public string Name { get; set; }
+        [BindProperty]
+    public int Age { get; set; }
+    public bool IsAdult { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if(Age >= 18)
+            {
+                IsAdult = true;
+            } else
+            {
+                IsAdult = false;
+            }
+        }
+    }
+}
