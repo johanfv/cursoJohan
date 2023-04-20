72400122af51e41f43f8cd2b08e6d0b5a3659f94
Johan Fonseca <jfonseca@grupocondal.cr>
2023-04-20 17:56:05 -0600

    Ejercicio 18

---
+++ 18-Javascript-Razor-Pages/18-Javascript-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _18_Javascript_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+            Name = "Johan";
+        }
+    }
+}
