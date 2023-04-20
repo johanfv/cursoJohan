c1b1d5ebba98b24cb797b05d5d1d9267f1028918
Johan Fonseca <jfonseca@grupocondal.cr>
2023-04-19 18:44:10 -0600

    ejemplo 16

---
+++ 16-ClassTransferData-Razor-Pages/16-ClassTransferData-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _16_ClassTransferData_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [ViewData]
+        public string Message { get; set; }
+        [BindProperty]
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            ViewData["MyString"] = "Cadena de text";
+            ViewData["Mynumber"] = 100;
+
+            Message = "Hello World!";
+        }
+
+        public IActionResult OnPost()
+        {
+            //Almacenar en TempData el valor Name que viene del formulario
+            //Para mostrarlo en una redirección
+            TempData["NameTempData"] = Name;
+            return RedirectToPage("/Privacy");
+        }
+    }
+}
