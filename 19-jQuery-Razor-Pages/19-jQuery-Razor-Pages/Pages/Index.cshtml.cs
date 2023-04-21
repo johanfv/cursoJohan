bda8a762056c7817c93ae35add8b7feac4c62ab8
Johan Fonseca <jfonseca@grupocondal.cr>
2023-04-20 18:29:14 -0600

    Ejercicio 19

---
+++ 19-jQuery-Razor-Pages/19-jQuery-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _19_jQuery_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string nombre { get; set; }
+        public void OnGet()
+        {
+            nombre = "Adalberto";
+        }
+    }
+}
