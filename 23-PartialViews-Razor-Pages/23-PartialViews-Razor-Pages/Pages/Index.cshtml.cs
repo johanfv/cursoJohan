903a84bf6407e5effad4a93bee82a0cc0de7dcf6
Johan Fonseca <johanfonsecavasquez@gmail.com>
2023-04-24 17:48:07 -0600

    ejercicio 23

---
+++ 23-PartialViews-Razor-Pages/23-PartialViews-Razor-Pages/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _23_PartialViews_Razor_Pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<string> Animals = new List<string>();
+
+        public void OnGet()
+        {
+            Animals.AddRange(new[]
+           {
+                "Perro",
+                "Gato",
+                "Caballo",
+                "Tigre"
+           });
+        }
+    }
+}
